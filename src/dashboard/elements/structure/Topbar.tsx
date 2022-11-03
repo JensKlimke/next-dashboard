ffe5a3ee83c1588a2f08820b4185282ada75a0a6
Jens Klimke <jens.klimke@rwth-aachen.de>
2022-11-03 07:47:06 +0100

    don't show UserMenu, when no user

--- src/dashboard/elements/structure/Topbar.tsx
+++ src/dashboard/elements/structure/Topbar.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import {Button, Container, Image, Nav, Navbar, NavDropdown, NavLink} from 'react-bootstrap';
+import {Button, Container, Image, Navbar} from 'react-bootstrap';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faExpand} from '@fortawesome/free-solid-svg-icons';
 import {usePage} from "dashboard/contexts/page";
 import UserMenu from "dashboard/elements/profile/UserMenu";
 import {useConfig} from "dashboard/contexts/config";
+import {useAuth} from "auth/contexts/auth";
 
 export default function Topbar({onOpenKiosk} : { onOpenKiosk: () => void }) {
+  // get contexts
   const page = usePage();
   const config = useConfig();
+  const {user} = useAuth();
   // render topbar
   return (
     <Navbar bg='light' expand='lg' className='Topbar'>
@@ -23,7 +26,7 @@ export default function Topbar({onOpenKiosk} : { onOpenKiosk: () => void }) {
           <FontAwesomeIcon icon={faExpand} />
         </Button>
         <Navbar.Toggle aria-controls='navbarScroll' />
-        <UserMenu />
+        { user && <UserMenu /> }
       </Container>
     </Navbar>
   );
